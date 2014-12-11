de0eef6efb17a4ca89bc4222eb2bbab836d15d34
Mike Howles <entmike@gmail.com>
2014-12-10 18:05:57 -0600

    Fiori AppHeader APS Completion using ListBuilder
    Including sdk.xsd for new SDK developer convenience
    
    Added designstudio.target to basics plugin so new developers can
    reference it like in SDK documentation.

--- src/org.scn.community.basics/res/FioriAppHeader/aps/FioriAppHeaderPropertyPage.js
+++ src/org.scn.community.basics/res/FioriAppHeader/aps/FioriAppHeaderPropertyPage.js
@@ -76,6 +76,16 @@ sap.designstudio.sdk.PropertyPage.subclass("org.scn.community.basics.FioriAppHea
 		this.firePropertiesChanged(["itemConfig"]);
 		this.updateProps();
 	};
+	/*
+	 * Close Item Properties Popup
+	 */
+	this.closeDetail = function(oControlEvent){
+		if(this._popup) {
+			this._popup.close();
+			this._popup.destroy();
+			delete this._popup;
+		}
+	};
 	this.moveItem = function(oControlEvent){
 		var section = this.getSection(this.actionList.getSelectedKey());
 		if(!section) return;
@@ -129,7 +139,36 @@ sap.designstudio.sdk.PropertyPage.subclass("org.scn.community.basics.FioriAppHea
 				this.updateProps();
 			}
 		}	
-	};	
+	};
+	this.itemDetailsChanged = function(oControlEvent){	
+		// Protect Key
+		var allItems = new org.scn.community.propertysheet.ListBuilder();		
+		allItems.setList(this.getSection(this._selectedSectionKey).items);
+		if(this._currentItemConfig.key != this.txtItemKey.getValue()) this._currentItemConfig.key = allItems.generateKey(this.txtItemKey.getValue());
+		delete allItems;	
+		this._currentItemConfig.text = this.txtItemTitle.getValue();
+		this._currentItemConfig.url = this.txtItemUrl.getValue();
+		this.firePropertiesChanged(["itemConfig"]);
+		this.updateProps();
+		this.closeDetail();
+	}
+	this.showItem = function(oControlEvent){
+		var detailData = oControlEvent.getParameters();
+		var section = this.getSection(this._selectedSectionKey);
+		this._currentItemConfig = null;
+		for(var i=0;i<section.items.length;i++){
+			var item = section.items[i];
+			if(item.key == detailData.key) this._currentItemConfig = item;
+		}
+		if(!this._currentItemConfig) return;
+		this.txtItemKey.setValue(this._currentItemConfig.key);
+		this.txtItemTitle.setValue(this._currentItemConfig.text);
+		this.txtItemUrl.setValue(this._currentItemConfig.url);
+		if(!this._popup) this._popup = new sap.ui.core.Popup(this.itemDetailPanel, true, true, true);
+		
+		//this._popup.destroyContent();
+		this._popup.open(250,"center center", "center center", document.body, null);
+	};
 	this.init = function(){
 		// Build UI
 		this._content = new sap.ui.commons.layout.HorizontalLayout({ });
@@ -146,7 +185,8 @@ sap.designstudio.sdk.PropertyPage.subclass("org.scn.community.basics.FioriAppHea
 		this.txtActionIcon = new sap.ui.commons.TextField({});
 		this.lblActionItems = new sap.ui.commons.TextView({text : "Items"});
 		this.actionItems = new org.scn.community.propertysheet.ListBuilder({
-			width : "200px"
+			width : "200px",
+			showDetail : true
 		});
 		
 		this.actionProps.addContent(this.lblActionKey);
@@ -163,6 +203,41 @@ sap.designstudio.sdk.PropertyPage.subclass("org.scn.community.basics.FioriAppHea
 		this._content.addContent(this.actionProps);
 		this._content.placeAt($("#content"));
 
+		// Item Detail Components
+		this.itemDetailPanel = new sap.ui.commons.Panel({
+			text : "Item Details",
+			showCollapseIcon : false,
+			width : "80%"
+		});
+		this.itemDetailLayout = new sap.ui.commons.layout.VerticalLayout({
+			width : "100%"
+		});
+		this.detailButtons = new sap.ui.commons.layout.HorizontalLayout({ });
+		this.closeButton = new sap.ui.commons.Button({
+			text : "Cancel"
+		});
+		this.okButton = new sap.ui.commons.Button({
+			text : "Update"
+		});
+		this.detailButtons.addContent(this.closeButton);
+		this.detailButtons.addContent(this.okButton);
+		this.closeButton.attachPress(this.closeDetail,this);
+		this.okButton.attachPress(this.itemDetailsChanged,this);
+		this.itemKey = new sap.ui.commons.TextView({text : "Item Key"});
+		this.txtItemKey = new sap.ui.commons.TextField({ });
+		this.itemTitle = new sap.ui.commons.TextView({text : "Item Title"});
+		this.txtItemTitle = new sap.ui.commons.TextField({ });
+		this.itemUrl = new sap.ui.commons.TextView({text : "Item Image URL"});
+		this.txtItemUrl = new sap.ui.commons.TextField({ });
+		this.itemDetailLayout.addContent(this.itemKey);
+		this.itemDetailLayout.addContent(this.txtItemKey);
+		this.itemDetailLayout.addContent(this.itemTitle);
+		this.itemDetailLayout.addContent(this.txtItemTitle);
+		this.itemDetailLayout.addContent(this.itemUrl);
+		this.itemDetailLayout.addContent(this.txtItemUrl);
+		this.itemDetailLayout.addContent(this.detailButtons);
+		this.itemDetailPanel.addContent(this.itemDetailLayout);
+		
 		// Events
 		this.actionList.attachItemAdded(this.addSection,this);
 		this.actionList.attachItemSelected(this.showSection,this);
@@ -170,7 +245,7 @@ sap.designstudio.sdk.PropertyPage.subclass("org.scn.community.basics.FioriAppHea
 		this.actionList.attachItemMoved(this.moveSection,this);
 
 		this.actionItems.attachItemAdded(this.addItem,this);
-		//this.actionItems.attachItemSelected(this.showItem,this);
+		this.actionItems.attachItemDetail(this.showItem,this);
 		this.actionItems.attachItemDeleted(this.deleteItem,this);
 		this.actionItems.attachItemMoved(this.moveItem,this);
 
@@ -188,7 +263,7 @@ sap.designstudio.sdk.PropertyPage.subclass("org.scn.community.basics.FioriAppHea
 			var o = [];
 			if(s && s!="") o = jQuery.parseJSON(s);
 			this._items = o;
-			//this.updateProps();
+			this.updateProps();
 			return this;
 		}
 	};	
