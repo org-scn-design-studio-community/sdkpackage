313a151e432560aa0fd5e485b1fc036567b2472a
Mafi78 <manfred.schwarz@gmail.com>
2015-01-23 12:40:56 +0100

    Added hierarchical component to Data Bound project

--- src/org.scn.community.databound/script/build.cmd
+++ src/org.scn.community.databound/script/build.cmd
@@ -7,6 +7,7 @@ type ..\..\org.scn.community.shared\templates.main\contribution.xml.head.txt ^
 ..\templates\contribution.xml.groups.txt ^
 ..\res\DataTable\def\contribution.xml ^
 ..\res\DropDown\def\contribution.xml ^
+..\res\D3Hier\def\contribution.xml ^
 ..\res\LeaderBoard\def\contribution.xml ^
 ..\res\MultiLevelDropDown\def\contribution.xml ^
 ..\res\RangeSlider\def\contribution.xml ^
@@ -24,6 +25,7 @@ type ..\..\org.scn.community.shared\templates.main\contribution.ztl.head.txt ^
 ..\..\org.scn.community.shared\templates.main\license.js.txt ^
 ..\res\DataTable\def\contribution.ztl ^
 ..\res\DropDown\def\contribution.ztl ^
+..\res\D3Hier\def\contribution.ztl ^
 ..\res\LeaderBoard\def\contribution.ztl ^
 ..\res\MultiLevelDropDown\def\contribution.ztl ^
 ..\res\RangeSlider\def\contribution.ztl ^
