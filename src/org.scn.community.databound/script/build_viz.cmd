2908fa76551be114b82948cc76a8d0024b578153
Mike Howles <mike.howles@gmail.com>
2015-03-09 17:11:42 -0500

    Including Choropleth contribution.xml generation in .cmd file.

--- src/org.scn.community.databound/script/build_viz.cmd
+++ src/org.scn.community.databound/script/build_viz.cmd
@@ -37,3 +37,15 @@ type ..\res\MarimekkoChart\def\header.xml ^
 ..\res\MarimekkoChart\def\defaults.xml ^
 .\vizDefs\Footer.xml > ^
 ..\res\MarimekkoChart\def\contribution.xml
+
+del ..\res\Choropleth\def\contribution.xml
+type ..\res\Choropleth\def\header.xml ^
+.\vizDefs\Base.xml ^
+.\vizDefs\BaseViz.xml ^
+..\res\Choropleth\def\properties.xml ^
+.\vizDefs\InitializationHeader.xml ^
+.\vizDefs\Base.defaults.xml ^
+.\vizDefs\BaseViz.defaults.xml ^
+..\res\Choropleth\def\defaults.xml ^
+.\vizDefs\Footer.xml > ^
+..\res\Choropleth\def\contribution.xml
