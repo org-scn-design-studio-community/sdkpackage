366a2b6a81b77f50c69c538e9b9a0df754b2e7f3
Karol Kalisz <karol@kalisz.org>
2015-05-29 18:03:27 +0200

    - puhing changes for generated version of bullet chart

--- src/org.scn.community.prototypes/res/BulletChartGen/BulletChartGenSpec.js
+++ src/org.scn.community.prototypes/res/BulletChartGen/BulletChartGenSpec.js
@@ -20,19 +20,377 @@
 (function() {
 
 	org_scn_community_require.knownComponents.prototypes.BulletChartGen.spec = 
-{"graphTitle": {
-  "opts": {
-    "apsControl": "text",
-    "cat": "Visualization",
-    "desc": "Graph Title",
-    "tooltip": "the title for the graph",
-    "ztlFunction": "",
-    "ztlType": "String"
+{
+  "clickedgraphkey": {
+    "opts": {
+      "cat": "Visualization",
+      "desc": "Clicked Key",
+      "noAps": true,
+      "tooltip": "the unique key of the clicked graph",
+      "ztlFunction": "",
+      "ztlType": "String"
+    },
+    "type": "String",
+    "value": "",
+    "visible": true
   },
-  "type": "String",
-  "value": "",
-  "visible": true
-}};
+  "comparison": {
+    "opts": {
+      "apsControl": "String",
+      "cat": "Visualization",
+      "desc": "comparon values",
+      "tooltip": "Column holding compare values",
+      "ztlFunction": "",
+      "ztlType": "String"
+    },
+    "type": "true",
+    "value": "",
+    "visible": true
+  },
+  "data": {
+    "onChange": "flattenData",
+    "opts": {
+      "cat": "Data",
+      "desc": "Dataset",
+      "noAps": true,
+      "noZtl": true,
+      "tooltip": "Data from datasource",
+      "type": "data",
+      "value": null,
+      "ztlFunction": ""
+    },
+    "type": "ResultSet",
+    "value": "null",
+    "visible": true
+  },
+  "extrapolation": {
+    "opts": {
+      "apsControl": "String",
+      "cat": "Visualization",
+      "desc": "Extrapolation Values",
+      "tooltip": "The Column key value of the extrapolation values",
+      "ztlFunction": "",
+      "ztlType": "String"
+    },
+    "type": "String",
+    "value": "",
+    "visible": true
+  },
+  "graphTitle": {
+    "opts": {
+      "apsControl": "text",
+      "cat": "Visualization",
+      "desc": "Graph Title",
+      "tooltip": "the title for the graph",
+      "ztlFunction": "",
+      "ztlType": "String"
+    },
+    "type": "String",
+    "value": "",
+    "visible": true
+  },
+  "keydimension": {
+    "opts": {
+      "apsControl": "",
+      "cat": "Visualization",
+      "desc": "Unique key values",
+      "tooltip": "Dimension name of the column that holds unique key values for each graph",
+      "ztlFunction": "",
+      "ztlType": "String"
+    },
+    "type": "String",
+    "value": "",
+    "visible": true
+  },
+  "labeldimension": {
+    "opts": {
+      "apsControl": "",
+      "cat": "Label Settings",
+      "desc": "dimension holding label values",
+      "tooltip": "Dimension name of the column that holds label values",
+      "ztlFunction": "",
+      "ztlType": "String"
+    },
+    "type": "String",
+    "value": "",
+    "visible": true
+  },
+  "maxgraphheight": {
+    "opts": {
+      "apsControl": "String",
+      "cat": "Visualization",
+      "desc": "max height ",
+      "tooltip": "Maximum height of a single graph",
+      "ztlFunction": "",
+      "ztlType": "int"
+    },
+    "type": "int",
+    "value": "40",
+    "visible": true
+  },
+  "mingraphheight": {
+    "chartbottommargin": {
+      "opts": {
+        "apsControl": "String",
+        "cat": "Visualization",
+        "desc": "Bottom Margin",
+        "tooltip": "Bottom Margin",
+        "ztlFunction": "",
+        "ztlType": "int"
+      },
+      "type": "int",
+      "value": "40",
+      "visible": true
+    },
+    "chartleftmargin": {
+      "opts": {
+        "apsControl": "String",
+        "cat": "Visualization",
+        "desc": "Left Margin",
+        "tooltip": "Left Margin",
+        "ztlFunction": "",
+        "ztlType": "int"
+      },
+      "type": "int",
+      "value": "40",
+      "visible": true
+    },
+    "chartrightmargin": {
+      "opts": {
+        "apsControl": "String",
+        "cat": "Visualization",
+        "desc": "Right Margin",
+        "tooltip": "Right Margin",
+        "ztlFunction": "",
+        "ztlType": "int"
+      },
+      "type": "int",
+      "value": "40",
+      "visible": true
+    },
+    "charttopmargin": {
+      "opts": {
+        "apsControl": "String",
+        "cat": "Visualization",
+        "desc": "Top Margin",
+        "tooltip": "Top Margin",
+        "ztlFunction": "",
+        "ztlType": "int"
+      },
+      "type": "int",
+      "value": "40",
+      "visible": true
+    },
+    "columnmargin": {
+      "opts": {
+        "apsControl": "String",
+        "cat": "Visualization",
+        "desc": "horizontal margin between graphs",
+        "tooltip": "horizontal margin between graphs",
+        "ztlFunction": "",
+        "ztlType": "int"
+      },
+      "type": "int",
+      "value": "40",
+      "visible": true
+    },
+    "higherisbetter": {
+      "onSet": "higherBetter",
+      "opts": {
+        "apsControl": "checkbox",
+        "cat": "Data",
+        "desc": "higher is better",
+        "tooltip": "realization higher than target is better",
+        "ztlFunction": "",
+        "ztlType": "boolean"
+      },
+      "type": "boolean",
+      "value": true,
+      "visible": true
+    },
+    "numberofcolumns": {
+      "opts": {
+        "apsControl": "String",
+        "cat": "Visualization",
+        "desc": "number of columns",
+        "tooltip": "number of columns for the graphs",
+        "ztlFunction": "",
+        "ztlType": "int"
+      },
+      "type": "int",
+      "value": "40",
+      "visible": true
+    },
+    "opts": {
+      "apsControl": "String",
+      "cat": "Visualization",
+      "desc": "Minimum Height",
+      "tooltip": "Minimum height of a single graph",
+      "ztlFunction": "",
+      "ztlType": "int"
+    },
+    "rowmargin": {
+      "opts": {
+        "apsControl": "String",
+        "cat": "Visualization",
+        "desc": "vertical margin between graphs",
+        "tooltip": "vertical margin between graphs",
+        "ztlFunction": "",
+        "ztlType": "int"
+      },
+      "type": "int",
+      "value": "40",
+      "visible": true
+    },
+    "type": "int",
+    "value": "40",
+    "visible": true
+  },
+  "realization": {
+    "opts": {
+      "apsControl": "String",
+      "cat": "Column Selection",
+      "desc": "Realization Column",
+      "tooltip": "The Column key value of the realization values",
+      "ztlFunction": "",
+      "ztlType": "String"
+    },
+    "type": "String",
+    "value": "",
+    "visible": true
+  },
+  "showalert": {
+    "opts": {
+      "apsControl": "combobox",
+      "cat": "Data",
+      "desc": "When to show an alert",
+      "options": [
+        {
+          "key": "comparison",
+          "text": "worse than comparison"
+        },
+        {
+          "key": "thresh5",
+          "text": "worse than threshold 5"
+        },
+        {
+          "key": "thresh4",
+          "text": "worse than threshold 5"
+        },
+        {
+          "key": "thresh3",
+          "text": "worse than threshold 5"
+        },
+        {
+          "key": "thresh2",
+          "text": "worse than threshold 5"
+        },
+        {
+          "key": "thresh1",
+          "text": "worse than threshold 1"
+        }
+      ],
+      "tooltip": "Choose situation when to show alert",
+      "ztlFunction": "",
+      "ztlType": "Choice"
+    },
+    "type": "String",
+    "value": "worse than comparison",
+    "visible": true
+  },
+  "showaxis": {
+    "onSet": "showAxis",
+    "opts": {
+      "apsControl": "checkbox",
+      "cat": "Data",
+      "desc": "Show X axis",
+      "tooltip": "Whether to show the X axis numbers underneath the graph",
+      "ztlFunction": "",
+      "ztlType": "boolean"
+    },
+    "type": "boolean",
+    "value": true,
+    "visible": true
+  },
+  "sublabeldimension": {
+    "opts": {
+      "apsControl": "",
+      "cat": "Label Settings",
+      "desc": "Clicked Key",
+      "tooltip": "Dimension name of the column that holds sublabel values",
+      "ztlFunction": "",
+      "ztlType": "String"
+    },
+    "type": "String",
+    "value": "",
+    "visible": true
+  },
+  "threshold1": {
+    "opts": {
+      "apsControl": "String",
+      "cat": "Column Selection",
+      "desc": "threshold 1 values",
+      "tooltip": "first option to select threshold values",
+      "ztlFunction": "",
+      "ztlType": "String"
+    },
+    "type": "String",
+    "value": "",
+    "visible": true
+  },
+  "threshold2": {
+    "opts": {
+      "apsControl": "String",
+      "cat": "Column Selection",
+      "desc": "threshold 2 values",
+      "tooltip": "second option to select threshold values",
+      "ztlFunction": "",
+      "ztlType": "String"
+    },
+    "type": "String",
+    "value": "",
+    "visible": true
+  },
+  "threshold3": {
+    "opts": {
+      "apsControl": "String",
+      "cat": "Column Selection",
+      "desc": "threshold 3 values",
+      "tooltip": "third option to select threshold values",
+      "ztlFunction": "",
+      "ztlType": "String"
+    },
+    "type": "String",
+    "value": "",
+    "visible": true
+  },
+  "threshold4": {
+    "opts": {
+      "apsControl": "String",
+      "cat": "Column Selection",
+      "desc": "threshold 4 values",
+      "tooltip": "fourth option to select threshold values",
+      "ztlFunction": "",
+      "ztlType": "String"
+    },
+    "type": "true",
+    "value": "",
+    "visible": false
+  },
+  "threshold5": {
+    "opts": {
+      "apsControl": "String",
+      "cat": "Column Selection",
+      "desc": "threshold 5 values",
+      "tooltip": "fifth option to select threshold values",
+      "ztlFunction": "",
+      "ztlType": "String"
+    },
+    "type": "true",
+    "value": "",
+    "visible": false
+  }
+};
 
 	org_scn_community_require.knownComponents.prototypes.BulletChartGen.specAbout = 
 {
@@ -40,7 +398,7 @@
   "icon": "BulletChartGen.png",
   "title": "Bullet Chart (Gen)",
   "topics": [{
-    "content": "Allows view on data in form of a bullet chart.",
+    "content": "Allows you to compare realization with target, other periods and multiple thresholds",
     "title": "Bullet Chart"
   }]
 };
