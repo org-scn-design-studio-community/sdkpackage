54bc3a6492e11c443463517a0ac25d1e7c611a71
MartinPankraz <martin.pankraz@convista.com>
2015-07-07 18:30:05 -0500

    -sap.m fix added

--- src/org.scn.community.databound/res/MultiComboBox/MultiComboBox.js
+++ src/org.scn.community.databound/res/MultiComboBox/MultiComboBox.js
@@ -17,7 +17,22 @@
  * limitations under the License. 
  */
 jQuery.sap.require("sap.m.MultiComboBox");
-
+//mark forced re-load of sap.m events bundle  
+oCfgData = window["sap-ui-config"]  
+// check of the library sap.m is already in declared libs (to avoid second execution)  
+if(oCfgData.libs.indexOf("sap.m") == -1) {  
+  // append the information that sap.m is loaded  
+  oCfgData.libs = oCfgData.libs + ",sap.m";  
+  // load sap.m and sap.me  
+  var oCore = sap.ui.getCore();  
+  oCore.loadLibrary("sap.m");  
+  if(!sap.ui.Device.support.touch) {  
+    // unload events bundle  
+    jQuery.sap.unloadResources("jquery.sap.events.js", false, true, true);  
+    // reload events bundle to assure that sap.m events are active  
+    jQuery.sap.require("jquery.sap.events");  
+  }  
+}
 sap.m.MultiComboBox.extend("org.scn.community.databound.MultiComboBox", {
 
 //	setData : function(value) {
