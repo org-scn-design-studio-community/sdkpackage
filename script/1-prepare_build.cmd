d8f908dbb9a0988e29312609324add5d25b6df88
Karol Kalisz <karol@kalisz.org>
2015-07-16 00:28:17 +0200

    - updating prepare build to copy some files

--- script/1-prepare_build.cmd
+++ script/1-prepare_build.cmd
@@ -34,3 +34,13 @@ set module=org.scn.community.geovis
 cd ..\src\%module%\script\
 call build.cmd
 cd %current%
+
+copy ..\src\org.scn.community.basics\res\require_loader.js ..\src\org.scn.community.databound\res\require_loader.js /Y
+copy ..\src\org.scn.community.basics\res\require_loader.js ..\src\org.scn.community.utils\res\require_loader.js /Y
+copy ..\src\org.scn.community.basics\res\require_loader.js ..\src\org.scn.community.datasource\res\require_loader.js /Y
+copy ..\src\org.scn.community.basics\res\require_loader.js ..\src\org.scn.community.prototypes\res\require_loader.js /Y
+
+copy ..\src\org.scn.community.basics\aps\*.* ..\src\org.scn.community.databound\aps\ /Y
+copy ..\src\org.scn.community.basics\aps\*.* ..\src\org.scn.community.utils\aps\ /Y
+copy ..\src\org.scn.community.basics\aps\*.* ..\src\org.scn.community.datasource\aps\ /Y
+copy ..\src\org.scn.community.basics\aps\*.* ..\src\org.scn.community.prototypes\aps\ /Y
