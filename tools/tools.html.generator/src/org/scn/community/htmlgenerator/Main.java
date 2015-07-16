e855ec0a573fc49307fb2816aa5df2c547464740
Karol Kalisz <karol@kalisz.org>
2015-07-16 10:10:38 +0200

    - updating model generation for unified

--- tools/tools.html.generator/src/org/scn/community/htmlgenerator/Main.java
+++ tools/tools.html.generator/src/org/scn/community/htmlgenerator/Main.java
@@ -232,6 +232,7 @@ public class Main {
 		templateList = templateList.replace(" %COMPONENT_LIST_ENTRY_BASICS%", "");
 		templateList = templateList.replace(" %COMPONENT_LIST_ENTRY_DATABOUND%", "");
 		templateList = templateList.replace(" %COMPONENT_LIST_ENTRY_CHARTMAPS%", "");
+		templateList = templateList.replace(" %COMPONENT_LIST_ENTRY_UNIFIED%", "");
 		templateList = templateList.replace(" %COMPONENT_LIST_ENTRY_DATASOURCE%", "");
 		templateList = templateList.replace(" %COMPONENT_LIST_ENTRY_GEO%", "");
 		templateList = templateList.replace(" %COMPONENT_LIST_ENTRY_PROTOTYPES%", "");
