9e9b98ea219c458cf8278cfaa23a40cf0c1bc7ab
Franck BLAIS <franck.blais@gmail.com>
2015-12-16 14:46:29 -0500

    Embeded Frame: Do not override window.domain if relax=0
    We had a issue at our customer with this setting ...

--- src/org.scn.community.basics/res/EmbeddedFrame/EmbeddedFrame.js
+++ src/org.scn.community.basics/res/EmbeddedFrame/EmbeddedFrame.js
@@ -64,22 +64,28 @@ EmbeddedFrame = function () {
 		if(this.skipport() && myHost.indexOf(":") > -1){
 			myHost = myHost.substring(0,myHost.indexOf(":"));
 		}
-		var pathArray = myHost.split('.');
-        var arrLength = pathArray.length;
-        if(arrLength>1){
-        	//extract subdomain in order to allow sth like:
-        	// Actual domain is "blah.bar.foo.com" 
-        	// document.domain = "bar.foo.com" 	// Ok 
-        	// document.domain = "foo.com" 		// Still ok 
-        	var domainName = "";
-        	var level = this.domainrelaxlevel();
-        	if(level < arrLength){
-        		domainName = pathArray.slice(level, arrLength).join('.');	
-        	}else{
-        		domainName = pathArray.join('.');
-        	}
-            //set relaxed domain for the current scope where document is active
-            document.domain = domainName;
+		
+		//Get Domain relax level
+		var level = this.domainrelaxlevel();
+		//if different from 0, apply. Else, do not change domain
+		if (level > 0) {
+			var pathArray = myHost.split('.');
+	        var arrLength = pathArray.length;
+	        if(arrLength>1){
+	        	//extract subdomain in order to allow sth like:
+	        	// Actual domain is "blah.bar.foo.com" 
+	        	// document.domain = "bar.foo.com" 	// Ok 
+	        	// document.domain = "foo.com" 		// Still ok 
+	        	var domainName = "";
+	        	
+	        	if(level < arrLength){
+	        		domainName = pathArray.slice(level, arrLength).join('.');	
+	        	}else{
+	        		domainName = pathArray.join('.');
+	        	}
+	            //set relaxed domain for the current scope where document is active
+	            document.domain = domainName;
+	        }
         }
 
 		this.$().html(html);
