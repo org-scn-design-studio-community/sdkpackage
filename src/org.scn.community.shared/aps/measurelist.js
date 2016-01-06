36cedf7fee80b96d71f86d535fe7384815df2062
pankraz <martin.pankraz@convista.com>
2016-01-06 11:55:10 +0100

    -missing measurelist.js added

---
+++ src/org.scn.community.shared/aps/measurelist.js
@@ -0,0 +1,52 @@
+/**
+ * Measure List Handler
+ */
+define([], function () {
+	/**
+	 * Measure List
+	 */
+	sap.ui.commons.ComboBox.extend("org.scn.community.aps.MeasureList", {
+		metadata : {
+			properties : {}
+		},
+		init : function () {
+			this.setDisplaySecondaryValues(true);
+			propertyPage.registerDataComponent(this);
+			if (propertyPage.rendered)
+				this.notifyDataChange();
+		},
+		notifyDataChange : function () {
+			this.destroyItems();
+			if (propertyPage && propertyPage.flatData) {
+				for (var i = 0; i < propertyPage.flatData.columnHeadersKeys.length; i++) {
+					var key = propertyPage.flatData.columnHeadersKeys[i];
+					var text = propertyPage.flatData.columnHeaders[i];
+					if (key) {
+						var newItem = new sap.ui.core.ListItem({
+								key : key,
+								text : text || key,
+								additionalText : key
+							});
+						this.addItem(newItem);
+					}
+				}
+			}
+		},
+		renderer : {}
+	});
+	return {
+		id : "measurelist",
+		serialized : true,
+		setter : function (property, value) {
+			this["cmp_" + property].setSelectedKey(newValue);
+		},
+		getter : function (property, control) {
+			return control.getSelectedKey();
+		},
+		createComponent : function (property, propertyOptions, changeHandler) {
+			var component = new org.scn.community.aps.MeasureList({ });
+			component.attachChange(changeHandler, this);
+			return component;
+		}
+	};
+});
