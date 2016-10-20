562020454d5447ea1e2150b7869b8232a229fb33
MartinPankraz <martin.pankraz@convista.com>
2016-10-20 18:10:18 +0200

    -comment updated

--- src/org.scn.community.utils/res/Audio/Audio.js
+++ src/org.scn.community.utils/res/Audio/Audio.js
@@ -15,6 +15,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
  * See the License for the specific language governing permissions and 
  * limitations under the License. 
+ * 
+ * This component uses howler.js: https://github.com/goldfire/howler.js
+ * 
  */
  
 //%DEFINE-START%
